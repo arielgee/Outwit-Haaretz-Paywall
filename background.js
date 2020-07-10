ce80af0828c7bbafb7ea964ce3e8438b25c92d69
arielgee <ariel.hazard@gmail.com>
2020-07-10 09:55:09 +0300

    Minor refactoring

--- background.js
+++ background.js
@@ -8,27 +8,27 @@
 	const RX_HAARETZ = new RegExp("^(" + HOST_HAARETZ + "/)([^\\?]*)(\\?.*)?$", "i");
 	const RX_THEMARKER = new RegExp("^(" + HOST_THEMARKER + "/)([^\\?]*)(\\?.*)?$", "i");
 
-	const URL_CDN_AMP_HRTZ = "https://www-haaretz-co-il.cdn.ampproject.org/v/s/www.haaretz.co.il/amp/";
-	const URL_CDN_AMP_MRKR = "https://www-themarker-com.cdn.ampproject.org/v/s/www.themarker.com/amp/";
+	const URL_CDN_HRTZ = "https://www-haaretz-co-il.cdn.ampproject.org/v/s/www.haaretz.co.il/amp/";
+	const URL_CDN_MRKR = "https://www-themarker-com.cdn.ampproject.org/v/s/www.themarker.com/amp/";
 
-	const QUERY_STRING = "?amp_js_v=0.1";
-	//const QUERY_STRING_FULL = "?usqp=mq331AQFKAGwASA%3D&amp_js_v=0.1#aoh=15939449457246&referrer=https%3A%2F%2Fwww.google.com&amp_tf=From%20%251%24s&ampshare=https%3A%2F%2Fwww.haaretz.co.il";
+	const QUERY_STRING_CDN = "?amp_js_v=0.1";
+	// const QUERY_STRING_CDN_FULL = "?usqp=mq331AQFKAGwASA%3D&amp_js_v=0.1#aoh=15939449457246&referrer=https%3A%2F%2Fwww.google.com&amp_tf=From%20%251%24s&ampshare=https%3A%2F%2Fwww.haaretz.co.il";
 
 	const CSS_RULE_BODY =	"html:not([amp4ads]) body {" +
-								"margin-right: 15% !important; " +
-								"margin-left: 40% !important; " +
-								"font-size: 100% !important; " +
-							"} ";
+								"margin-right: 15% !important;" +
+								"margin-left: 40% !important;" +
+								"font-size: 100% !important;" +
+							"}";
 
-	const CSS_RULE_HIDE_STUFF =	".c-list-honda, " +
-								".c-list-honda__items, " +
-								".u-autospace--2, " +
-								".u-type--epsilon, " +
-								"amp-sticky-ad.i-amphtml-layout-nodisplay, " +
-								"amp-embed[data-ampurl], " +
-								"amp-ad { " +
-									"display: none !important;" +
-								"} ";
+	// const CSS_RULE_HIDE_STUFF =	".c-list-honda," +
+	// 							".c-list-honda__items," +
+	// 							".u-autospace--2," +
+	// 							".u-type--epsilon," +
+	// 							"amp-sticky-ad.i-amphtml-layout-nodisplay," +
+	// 							"amp-embed[data-ampurl]," +
+	// 							"amp-ad { " +
+	// 								"display: none !important;" +
+	// 							"}";
 
 	const CSS_RULES_PRETTY_PAGE = CSS_RULE_BODY;	// CSS_RULE_HIDE_STUFF is removed by uBlock
 
@@ -48,8 +48,8 @@
 
 		const TABS_ON_UPDATED_FILTER = {
 			urls: [
-				URL_CDN_AMP_HRTZ + "*",
-				URL_CDN_AMP_MRKR + "*"
+				URL_CDN_HRTZ + "*",
+				URL_CDN_MRKR + "*"
 			],
 			properties: [ "status" ]
 		};
@@ -70,9 +70,9 @@
 			if(details.statusCode === 200) {
 
 				if(details.url.startsWith(HOST_HAARETZ)) {
-					objResolved = { redirectUrl: details.url.replace(RX_HAARETZ, URL_CDN_AMP_HRTZ + "$2" + QUERY_STRING) };
+					objResolved = { redirectUrl: details.url.replace(RX_HAARETZ, URL_CDN_HRTZ + "$2" + QUERY_STRING_CDN) };
 				} else if(details.url.startsWith(HOST_THEMARKER)) {
-					objResolved = { redirectUrl: details.url.replace(RX_THEMARKER, URL_CDN_AMP_MRKR + "$2" + QUERY_STRING) };
+					objResolved = { redirectUrl: details.url.replace(RX_THEMARKER, URL_CDN_MRKR + "$2" + QUERY_STRING_CDN) };
 				}
 			}
 			resolve(objResolved);
@@ -89,7 +89,7 @@
 	////////////////////////////////////////////////////////////////////////////////////
 	function onTabsAttached(tabId) {
 		browser.tabs.get(tabId).then((tab) => {
-			if (tab.url.startsWith(URL_CDN_AMP_HRTZ) || tab.url.startsWith(URL_CDN_AMP_MRKR) ) {
+			if (tab.url.startsWith(URL_CDN_HRTZ) || tab.url.startsWith(URL_CDN_MRKR) ) {
 				handleTabChangedState(tabId);
 			}
 		});
